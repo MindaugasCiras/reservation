eaa1b51b76fe7bdb195ae47d698357a5f8d9c018
Ciras <m.ciras@yahoo.com>
2023-04-22 23:35:18 +0300

    - add swagger

---
+++ src/main/java/com/ciras/reservation/config/WebSecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.ciras.reservation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz.antMatchers("/v2/api-docs",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**").permitAll()
+                .anyRequest().authenticated()
+        );
+        return http.build();
+    }
+
+}
